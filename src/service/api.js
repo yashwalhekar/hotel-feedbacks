f7e9342ff963e9746c1ea4d9948cdcbcac40abe4
yashwalhekar <yashwalhekar371@gmail.com>
2026-03-04 17:27:09 +0530

    add base url

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://billing-web-app-sdr9.onrender.com/api",
   // withCredentials: true,
 });
 
